ee2cc22781a79c99cdbcb618d6a5126344b2258e
Emilio Esteban <delhierroemilio@gmail.com>
2025-09-17 09:33:58 -0500

    arreglado metadatos y favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -84,13 +84,7 @@ export const metadata = {
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ], 
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
-  },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    themeColor: "#000000",
-  },
-  
+  },  
 };
 
 export default function RootLayout({ children }) {
